79bdc2468e00923f94bb060132368068dfa1618a
Nikolaj Dyring <nikodyring@gmail.com>
2017-11-13 11:32:20 +0100

    MainPage and AdminView
    Updated Mainpage to include navigation. Not fully functional yet. AdminView has got navigation as well.

---
+++ TouchpointApp/TouchpointApp/ViewModel/ViewModelBase.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace TouchpointApp.ViewModel
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+    }
+}
