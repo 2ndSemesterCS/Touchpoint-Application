bab19b924ab0de877e9ce45f5bc7b05adee3dc49
Nikolaj Dyring <nikodyring@gmail.com>
2017-11-15 13:57:26 +0100

    Kursist Model Class
    Model Class for Kursist.

---
+++ TouchpointApp/TouchpointApp/Model/Kursist.cs
@@ -0,0 +1,32 @@
+﻿namespace TouchpointApp.Model
+{
+    public class Kursist
+    {
+        // Instance Fields
+
+        private int _id;
+        private string _navn;
+        private string _eMail;
+        private string _by;
+        private string _land;
+
+        // Properties
+
+        public int ID { get; set; }
+        public string Navn { get; set; }
+        public string EMail { get; set; }
+        public string By { get; set; }
+        public string Land { get; set; }
+
+        // Constructor
+
+        public Kursist(int id, string navn, string email, string by, string land)
+        {
+            id = _id;
+            navn = _navn;
+            email = _eMail;
+            by = _by;
+            land = _land;
+        }
+    }
+}
