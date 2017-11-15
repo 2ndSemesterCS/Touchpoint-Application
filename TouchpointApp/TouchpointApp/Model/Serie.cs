4037fccebe8b8f548dd1433b95f51c049bff4a53
DunkRing <Chris31@live.dk>
2017-11-15 13:43:38 +0100

    Serie modelclass
    bum

---
+++ TouchpointApp/TouchpointApp/Model/Serie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TouchpointApp.Model
+{
+    class Serie
+    {
+        private int _serieId;
+        private Kursus _kursus;
+        private string _beskrivelse;
+
+        public int SerieID { get; set; }
+        public Kursus Kursus { get; set; }
+        public string Beskrivelse { get; set; }
+
+        public Serie(int SerieID, Kursus Kursus, string Beskrivelse)
+        {
+            SerieID = _serieId;
+            Kursus = _kursus;
+            Beskrivelse = _beskrivelse;
+        }
+    }
+}
