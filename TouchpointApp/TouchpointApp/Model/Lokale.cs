3132898550c51a8bb836770dfc35aa81ebbf35e8
IanMFrost <ianmfrost0@gmail.com>
2017-11-15 12:32:53 +0100

    Underviser, lokale og Undervisningssted  model class
    Oprettet model classes med constructor og properties

---
+++ TouchpointApp/TouchpointApp/Model/Lokale.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TouchpointApp.Model
+{
+    class Lokale
+    {
+        private string _navn;
+        private int _lokaleId;
+
+        public int LokaleId { get; set; }
+
+        public string Navn { get; set; }
+
+        public Lokale(int LokaleId, string Navn)
+        {
+            Navn = _navn;
+            LokaleId = _lokaleId;
+        }
+    }
+}
---
+++ TouchpointApp/TouchpointApp/Model/Underviser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TouchpointApp.Model
+{
+    class Underviser
+    {
+        private string _navn;
+        private int _underviserId;
+
+
+        public int UnderviserId { get; set; }
+
+        public string navn { get; set; }
+
+
+
+
+        public Underviser(string navn, int UnderviserId)
+        {
+            UnderviserId = _underviserId;
+            navn = _navn;
+        }
+
+
+
+    }
+}
