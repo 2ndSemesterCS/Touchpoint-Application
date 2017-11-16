5058f7116e8ea8bfccc5300d7988b6eea0f869a0
wandriey <jona434g@edu.easj.dk>
2017-11-16 15:42:08 +0100

    OpretUnderviserViewLavet
    Opret underviser viewet er lavet

--- TouchpointApp/TouchpointApp/Model/Underviser.cs
+++ TouchpointApp/TouchpointApp/Model/Underviser.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,12 +12,16 @@ namespace TouchpointApp.Model
         public int UnderviserId { get; set; }
 
         public string Navn { get; set; }
+        public string Addresse { get; set; }
+        public string Email { get; set; }
 
 
-        public Underviser(string navn, int underviserId)
+        public Underviser(string navn, string addresse, string email, int underviserId)
         {
             UnderviserId = underviserId;
             Navn = navn;
+            Addresse = addresse;
+            Email = email;
         }
 
 
