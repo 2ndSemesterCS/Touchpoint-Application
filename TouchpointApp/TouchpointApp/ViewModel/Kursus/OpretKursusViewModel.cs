aa171c55089f2499e40f3aaf0c9a54f80db86055
Nikolaj Dyring Jensen <nikodyring@gmail.com>
2017-11-18 23:53:30 +0100

    Ordnet Mappestruktur og opdateret projekttypen.

---
+++ TouchpointApp/TouchpointApp/ViewModel/Kursus/OpretKursusViewModel.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TouchpointApp.Model;
+
+namespace TouchpointApp.ViewModel
+{
+    public class OpretKursusViewModel : ViewModelBase
+    {
+        #region Instance Fields
+        private string _kursusTitel;
+        private string _kursusDato;
+        private string _kursusTidspunkt;
+        private string _kursusVarighed;
+
+        private double _kursusPris;
+
+        private Underviser _kursusUnderviser;
+        private Undervisningssted _kursusUndervisningssted;
+        #endregion  
+
+        #region Properties
+        public string KursusTitel
+        {
+            get { return _kursusTitel; }
+            set
+            {
+                _kursusTitel = value;
+                OnPropertyChanged("KursusTitel");
+            }
+        }
+        public string KursusDato
+        {
+            get { return _kursusDato; }
+            set
+            {
+                _kursusDato = value;
+                OnPropertyChanged("KursusDato");
+            }
+        }
+        public string KursusTidspunkt
+        {
+            get { return _kursusTidspunkt; }
+            set
+            {
+                _kursusTidspunkt = value;
+                OnPropertyChanged("KursusTitel");
+            }
+        }
+        public string KursusVarighed
+        {
+            get { return _kursusVarighed; }
+            set
+            {
+                _kursusVarighed = value;
+                OnPropertyChanged("KursusVarighed");
+            }
+        }
+        public double KursusPris
+        {
+            get { return _kursusPris; }
+            set
+            {
+                _kursusPris = value;
+                OnPropertyChanged("KursusPris");
+            }
+        }
+        public Underviser KursusUnderviser
+        {
+            get { return _kursusUnderviser; }
+            set
+            {
+                _kursusUnderviser = value;
+                OnPropertyChanged("KursusUnderviser");
+            }
+        }
+        public Undervisningssted KursusUndervisningssted
+        {
+            get { return _kursusUndervisningssted; }
+            set
+            {
+                _kursusUndervisningssted = value;
+                OnPropertyChanged("KursusUndervisningssted");
+            }
+        }
+        #endregion 
+    }
+}
