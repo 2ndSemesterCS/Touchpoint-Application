7beea6dc857d576b5d5fd5648aaca97276abb839
wandriey <jona434g@edu.easj.dk>
2017-11-19 15:03:19 +0100

    Opret underviser view
    Viewet til at oprette en underviser er lavet.

--- TouchpointApp/TouchpointApp/Model/Underviser.cs
+++ TouchpointApp/TouchpointApp/Model/Underviser.cs
@@ -4,25 +4,49 @@ using System.Linq;
 using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Security.Cryptography.Core;
 
 namespace TouchpointApp.Model
 {
     public class Underviser
     {
-        public int UnderviserId { get; set; }
-
-        public string Navn { get; set; }
-        public string Addresse { get; set; }
-        public string Email { get; set; }
-
+        private int _undervisningsid;
+        private string _navn;
+        private string _addresse;
+        private string _email;
 
         public Underviser(string navn, string addresse, string email, int underviserId)
         {
-            UnderviserId = underviserId;
-            Navn = navn;
-            Addresse = addresse;
-            Email = email;
+            _undervisningsid = underviserId;
+            _navn = navn;
+            _addresse = addresse;
+            _email = email;
         }
+
+        public int UnderviserId
+        {
+            get { return _undervisningsid; }
+            set { _undervisningsid = value; }
+        }
+
+        public string Navn
+        {
+            get { return _navn; }
+            set { _navn = value; }
+        }
+        public string Addresse
+        {
+            get { return _addresse; }
+            set { _addresse = value; }
+        }
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+
+
+       
 
 
 
