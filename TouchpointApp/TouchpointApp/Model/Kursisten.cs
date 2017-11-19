ad8220c865c639aad296e17fd9e79157befd19d0
Kasper Kofoed <kasp5553@edu.easj.dk>
2017-11-19 16:44:07 +0100

    ModelClass Kursisten & ViewModelClass OpretKursistViewModel
    Oprettet modelclass til kursist og viewmodel

---
+++ TouchpointApp/TouchpointApp/Model/Kursisten.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TouchpointApp.Model
+{
+    class Kursisten
+    {
+        private string _navn;
+        private string _telefon;
+        private string _email;
+        private int _kursistId;
+
+        public int KursistId { get; set; }
+
+        public string Email { get; set; }
+
+        public string Telefon { get; set; }
+
+        public string Navn { get; set; }
+
+
+        public Kursisten(int kursistId, string telefon, string email, string navn)
+        {
+            Telefon = telefon;
+            KursistId = kursistId;
+            Email = email;
+            Navn = navn;
+
+        }
+    }
+}
+}
