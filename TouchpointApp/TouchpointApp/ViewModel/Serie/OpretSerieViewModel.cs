937ef84a2f531d346847a2a1f984d77e09849518
DunkRing <chris31@live.dk>
2017-11-19 15:22:41 +0100

    OpretSerieVeiwModel

---
+++ TouchpointApp/TouchpointApp/ViewModel/Serie/OpretSerieViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TouchpointApp.Model;
+
+namespace TouchpointApp.ViewModel
+{
+    public class OpretSerieViewModel : ViewModelBase
+    {
+        #region Instance Fields
+        private string _beskrivelse;
+
+        private int _serieId;
+
+        private Model.Kursus _kursus;
+
+        #endregion
+
+        #region Properties
+        public string Beskrivelse
+        {
+            get { return _beskrivelse; }
+            set
+            {
+                _beskrivelse = value;
+                OnPropertyChanged("Beskrivelse");
+            }
+        }
+        public int SerieID
+        {
+            get { return _serieId; }
+            set
+            {
+                _serieId = value;
+                OnPropertyChanged("SerieID");
+            }
+        }
+        public Model.Kursus Kursus
+        {
+            get { return _kursus; }
+            set
+            {
+                _kursus = value;
+                OnPropertyChanged("Kursus");
+            }
+
+            #endregion
+        }
+    }
+}
