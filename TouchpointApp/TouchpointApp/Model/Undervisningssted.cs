1a0b2c9c7415fef8bcfa8a3e9139aafa8bfef491
wandriey <jona434g@edu.easj.dk>
2017-11-20 11:58:35 +0100

    Rettelser af undervisningssted model
    Fejl ved merge rettet.

--- TouchpointApp/TouchpointApp/Model/Undervisningssted.cs
+++ TouchpointApp/TouchpointApp/Model/Undervisningssted.cs
@@ -9,25 +9,21 @@ namespace TouchpointApp.Model
 {
     public class Undervisningssted
     {
-<<<<<<< HEAD
-        private string _tlf;
-        private string _email;
-        private string _adresse;
-
-        public string Tlf{ get; set; }
-        public string Email { get; set; }
-        public string Adresse { get; set; }
-
-        public Undervisningssted(string tlf, string email, string adresse)
-        {
-            _tlf = tlf;
-            _email = email;
-=======
         private string _adresse;
         private Lokale _lokale;
+
+
+        public Undervisningssted(Lokale lokale, string adresse)
+        {
+          
+          _lokale = lokale;
+            _adresse = adresse;
+        }
+
+
+      
         public Undervisningssted(string adresse, Model.Lokale lokale)
         {
->>>>>>> 3b5821b60f7f252deefbee21e471d401f567c711
             _adresse = adresse;
             _lokale = lokale;
         }
