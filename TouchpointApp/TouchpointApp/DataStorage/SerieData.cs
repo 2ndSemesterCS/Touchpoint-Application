5da238ee93eb66bd27b17ace3583272f129c6ae5
DunkRing <Chris31@live.dk>
2017-11-27 14:11:18 +0100

    DataSerie
    asd

--- TouchpointApp/TouchpointApp/DataStorage/SerieData.cs
+++ TouchpointApp/TouchpointApp/DataStorage/SerieData.cs
@@ -1,7 +1,35 @@
-﻿namespace TouchpointApp.DataStorage
+﻿using TouchpointApp.Persistency;
+using TouchpointApp.Model;
+
+namespace TouchpointApp.DataStorage
 {
     public class SerieData
     {
+        #region Instance fields
+        private string _beskrivelse;
+
+        private Kursus _kursus;
+        #endregion
+
+        #region Propperties
+        public string Beskrivelse
+        {
+            get { return _beskrivelse; }
+            set
+            {
+                _beskrivelse = value;
+               
+            }
+        }
         
-    }
+        public Model.Kursus Kursus
+        {
+            get { return _kursus; }
+            set
+            {
+                _kursus = value;
+             
+            }
+            #endregion
+        }
 }
