0589ad99d0b42aa8bbe6142e06d7b25c4115d789
Kasper Kofoed <kasp5553@edu.easj.dk>
2017-11-27 14:05:00 +0100

    + Class "LokaleData"

---
+++ TouchpointApp/TouchpointApp/DataStorage/LokaleData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TouchpointApp.DataStorage
+{
+    class LokaleData
+    {
+         #region Instance Fields
+        private string _navn;
+        private int _lokaleId;
+        #endregion
+
+         #region Properties
+        public string Navn
+        {
+            get { return _navn; }
+            set { _navn = value;}
+        }
+
+        public int LokaleId
+        {
+            get { return _lokaleId; }
+            set { _lokaleId = value; }
+        }
+        #endregion
+    }
+}
