3190c0876012306697a7e9b8d234ecbf15e4204a
Kasper Kofoed <kasp5553@edu.easj.dk>
2017-11-27 14:21:34 +0100

    + UndervisningsstedData / rettet LokaleData
    .

--- TouchpointApp/TouchpointApp/DataStorage/LokaleData.cs
+++ TouchpointApp/TouchpointApp/DataStorage/LokaleData.cs
@@ -8,22 +8,15 @@ namespace TouchpointApp.DataStorage
 {
     class LokaleData
     {
-         #region Instance Fields
+        #region Instance Fields
         private string _navn;
-        private int _lokaleId;
         #endregion
 
          #region Properties
         public string Navn
         {
             get { return _navn; }
-            set { _navn = value;}
-        }
-
-        public int LokaleId
-        {
-            get { return _lokaleId; }
-            set { _lokaleId = value; }
+            set { _navn = value; }
         }
         #endregion
     }
