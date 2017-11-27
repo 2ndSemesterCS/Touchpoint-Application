8c2cc0bd1356accb6fb77abd9317361fb7d386a3
Nikolaj Dyring <nikodyring@gmail.com>
2017-11-27 15:02:58 +0100

    KursusData

--- TouchpointApp/TouchpointApp/ViewModel/Kursus/KursusViewModel.cs
+++ TouchpointApp/TouchpointApp/ViewModel/Kursus/KursusViewModel.cs
@@ -1,122 +1,23 @@
 ﻿using TouchpointApp.Command;
+using TouchpointApp.DataStorage;
 using TouchpointApp.Persistency;
 
 namespace TouchpointApp.ViewModel.Kursus
 {
     public class KursusViewModel : ViewModelBase
     {
-        #region Instance Fields
 
         private KursusCatalog _kc;
-
-        private string _kursusTitel;
-        private string _kursusDato;
-        private string _kursusTidspunkt;
-        private string _kursusVarighed;
-        private string _kursusSprog;
-        private string _beskrivelse;
-
-        private double _depositum;
-        private double _kursusPris;
-
-        private Model.Underviser _kursusUnderviser;
-        private Model.Undervisningssted _kursusUndervisningssted;
-        #endregion  
+        private KursusData _nytKursus;
 
         #region Properties
-        public string KursusTitel
+
+        public KursusData NytKursus
         {
-            get { return _kursusTitel; }
-            set
-            {
-                _kursusTitel = value;
-                OnPropertyChanged("KursusTitel");
-            }
-        }
-        public string KursusDato
-        {
-            get { return _kursusDato; }
-            set
-            {
-                _kursusDato = value;
-                OnPropertyChanged("KursusDato");
-            }
-        }
-        public string KursusTidspunkt
-        {
-            get { return _kursusTidspunkt; }
-            set
-            {
-                _kursusTidspunkt = value;
-                OnPropertyChanged("KursusTitel");
-            }
-        }
-        public string KursusVarighed
-        {
-            get { return _kursusVarighed; }
-            set
-            {
-                _kursusVarighed = value;
-                OnPropertyChanged("KursusVarighed");
-            }
-        }
-        public double KursusPris
-        {
-            get { return _kursusPris; }
-            set
-            {
-                _kursusPris = value;
-                OnPropertyChanged("KursusPris");
-            }
-        }
-        public Model.Underviser KursusUnderviser
-        {
-            get { return _kursusUnderviser; }
-            set
-            {
-                _kursusUnderviser = value;
-                OnPropertyChanged("KursusUnderviser");
-            }
-        }
-        public Model.Undervisningssted KursusUndervisningssted
-        {
-            get { return _kursusUndervisningssted; }
-            set
-            {
-                _kursusUndervisningssted = value;
-                OnPropertyChanged("KursusUndervisningssted");
-            }
+            get { return _nytKursus; }
+            set { }
         }
 
-        public string Sprog
-        {
-            get { return _kursusSprog; }
-            set
-            {
-                _kursusSprog = value;
-                OnPropertyChanged("KursusSprog");
-            }
-        }
-
-        public string Beskrivelse
-        {
-            get { return _beskrivelse; }
-            set
-            {
-                _beskrivelse = value;
-                OnPropertyChanged("Beskrivelse");
-            }
-        }
-
-        public double Depositum
-        {
-            get { return _depositum; }
-            set
-            {
-                _depositum = value;
-                OnPropertyChanged("Depositum");
-            }
-        }
         #endregion
 
         #region Commands
@@ -129,6 +30,7 @@ namespace TouchpointApp.ViewModel.Kursus
         public KursusViewModel()
         {
             _kc = new KursusCatalog();
+            _nytKursus = new KursusData();
             OpretNytKursusCommand = new RelayCommand(OpretNytKursus);
         }
         #endregion
@@ -137,7 +39,7 @@ namespace TouchpointApp.ViewModel.Kursus
 
         private void OpretNytKursus()
         {
-            _kc.OpretKursus(_kursusTitel, _kursusDato, _kursusTidspunkt, _kursusVarighed, _kursusPris, _kursusUnderviser, _kursusUndervisningssted, _kursusSprog, _beskrivelse, _depositum);
+            _kc.OpretKursus(_nytKursus.KursusTitel, _nytKursus.KursusDato, _nytKursus.KursusTidspunkt, _nytKursus.KursusVarighed, _nytKursus.KursusPris, _nytKursus.KursusUnderviser, _nytKursus.KursusUndervisningssted, _nytKursus.Sprog, _nytKursus.Beskrivelse, _nytKursus.Depositum);
 
         }
 
