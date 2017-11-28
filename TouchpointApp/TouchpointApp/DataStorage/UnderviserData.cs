672842f4374dee0c47f5c9dbed95be4c1d0b0582
wandriey <jona434g@edu.easj.dk>
2017-11-28 09:52:50 +0100

    Opret underviser Viewmodel

--- TouchpointApp/TouchpointApp/DataStorage/UnderviserData.cs
+++ TouchpointApp/TouchpointApp/DataStorage/UnderviserData.cs
@@ -2,6 +2,21 @@
 {
     public class UnderviserData
     {
-        
+        private string _navn;
+        private string _addresse;
+        private string _email;
+        private string _tlf;
+
+        public UnderviserData()
+        {
+
+        }
+
+        public string Navn { get { return _navn; } set { _navn = value; } }
+        public string Adresse { get { return _addresse; } set { _addresse = value; } }
+        public string Email { get { return _email; } set { _email = value; } }
+        public string Tlf { get { return _email; } set { _email = value; } }
+
+
     }
 }
--- TouchpointApp/TouchpointApp/Persistency/UnderviserCatalog.cs
+++ TouchpointApp/TouchpointApp/Persistency/UnderviserCatalog.cs
@@ -1,10 +1,34 @@
-﻿namespace TouchpointApp.Persistency
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TouchpointApp.Command;
+using TouchpointApp.Model;
+
+namespace TouchpointApp.Persistency
 {
     public class UnderviserCatalog
     {
+        #region Instance Fields
+        private List<Underviser> _Ul;
+        #endregion
+
+        #region Constructor
         public UnderviserCatalog()
         {
-
+            _Ul = new List<Underviser>();
         }
+        #endregion
+
+        #region Metoder
+        public void OpretKursus(string Navn, string Adresse, string email, string tlf)
+        {
+            Underviser U1 = new Underviser(Navn, Adresse, email, tlf);
+            _Ul.Add(U1);
+        }
+        #endregion
     }
 }
+
+
