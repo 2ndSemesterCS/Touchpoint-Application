4d701a3ea7313354a0b6d5cef2bfdf8c5daa6308
IanMFrost <ianmfrost0@gmail.com>
2017-11-28 16:24:52 +0100

    Opret ny Kursist done

--- TouchpointApp/TouchpointApp/DataStorage/KursistData.cs
+++ TouchpointApp/TouchpointApp/DataStorage/KursistData.cs
@@ -1,51 +1,62 @@
 ﻿using TouchpointApp.Persistency;
+using TouchpointApp.ViewModel;
 namespace TouchpointApp.DataStorage
 {
-    public class KursistData
+    public class KursistData : ViewModelBase
     {
         #region Instance Fields
-
-        private Model.Kursist _kursist;
-        private KursistCatalog _kursistCatalog;
+        
         private string _email;
         private string _tlf;
         private string _navn;
 
 #endregion
 
+
+        public KursistData ()
+        {
+
+        }
+
         #region Properties
-        public string Email
-        {
-            get
-            {
-                return _kursist.EMail;
-            }
-            set
-            {
-                _kursist.EMail = value;
-             
-            }
-        }
 
-        public string Telefon
-        {
-            get { return _kursist.Tlf; }
-            set
-            {
-                _kursist.Tlf = value;
-                
-            }
-        }
+        public string Email { get { return _email; } set { _email = value; OnPropertyChanged("Email"); } }
 
-        public string Navn
-        {
-            get { return _kursist.Navn; }
-            set
-            {
-                _kursist.Navn = value;
-                ;
-            }
-        }
+        public string Navn { get { return _navn; } set { _navn = value; OnPropertyChanged("navn"); } }
+       
+        public string Tlf { get { return _tlf; } set { _tlf = value; OnPropertyChanged("tlf"); } }
+        //public string Email
+        //{
+        //    get
+        //    {
+        //        return _kursist.EMail;
+        //    }
+        //    set
+        //    {
+        //        _kursist.EMail = value;
+
+        //    }
+        //}
+
+        //public string Telefon
+        //{
+        //    get { return _kursist.Tlf; }
+        //    set
+        //    {
+        //        _kursist.Tlf = value;
+
+        //    }
+        //}
+
+        //public string Navn
+        //{
+        //    get { return _kursist.Navn; }
+        //    set
+        //    {
+        //        _kursist.Navn = value;
+        //        ;
+        //    }
+        //}
 
     }
 #endregion
--- TouchpointApp/TouchpointApp/ViewModel/Kursist/KursistViewModel.cs
+++ TouchpointApp/TouchpointApp/ViewModel/Kursist/KursistViewModel.cs
@@ -12,52 +12,19 @@ namespace TouchpointApp.ViewModel.Kursist
         private KursistCatalog _kc;
         private KursistData _kd;
 
-        private Model.Kursist _kursist;
-        private KursistCatalog _kursistCatalog;
-
-        #endregion
-
-        #region Properties
-        public string Email
-        {
-            get
-            {
-                return _kursist.EMail;
-            }
-            set
-            {_kursist.EMail = value;
-                OnPropertyChanged("Email");
-            }
-        }
-
-        public string Telefon
-        {
-            get { return _kursist.Tlf; }
-            set
-            {
-                _kursist.Tlf = value;
-                OnPropertyChanged("Telefon");
-            }
-        }
-
-        public string Navn
-        {
-            get { return _kursist.Navn; }
-            set { _kursist.Navn = value;
-                OnPropertyChanged("Navn"); }
-        }
-
         #endregion
 
         #region Commands
         public RelayCommand OpretNyKursistCommand { get; set; }
+
+        public KursistData KursistData { get { return _kd; } set { _kd = value; } }
         #endregion
 
         #region Constructor
 
         public KursistViewModel()
         {
-            _kursistCatalog = new KursistCatalog();
+            _kc = new KursistCatalog();
             _kd = new KursistData();
 
             OpretNyKursistCommand = new RelayCommand(OpretNyKursist);
@@ -68,7 +35,7 @@ namespace TouchpointApp.ViewModel.Kursist
 
         public void OpretNyKursist()
         {
-            _kc.OpretKursist(_kd.Navn,_kd.Email,_kd.Telefon);
+            _kc.OpretKursist(_kd.Navn,_kd.Email,_kd.Tlf);
         }
 
         #endregion
