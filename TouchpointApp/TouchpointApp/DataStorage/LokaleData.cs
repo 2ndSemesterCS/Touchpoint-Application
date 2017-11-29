11b875881284e33dcc0f22fb3f8e0ff43ff07e29
Kasper Kofoed <kasp5553@edu.easj.dk>
2017-11-29 10:33:50 +0100

    + DATA BRO
    ayeeeeeeee

---
+++ TouchpointApp/TouchpointApp/DataStorage/LokaleData.cs
@@ -0,0 +1,17 @@
+﻿using TouchpointApp.ViewModel;
+
+namespace TouchpointApp.DataStorage
+{
+    public class LokaleData : ViewModelBase
+    {
+        private string _navn;
+
+        public LokaleData()
+        {
+
+        }
+
+        public string Navn { get { return _navn; } set { _navn = value; OnPropertyChanged("navn"); } }
+
+    }
+}
