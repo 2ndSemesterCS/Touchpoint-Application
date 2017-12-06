6cc714f1d9b2c1de304565543c76d014f56823f5
wandriey <jona434g@edu.easj.dk>
2017-12-06 11:35:09 +0100

    Rediger underviser viewmodel virker.

--- TouchpointApp/TouchpointApp/ViewModel/Underviser/UnderviserViewmodelCollectionRediger.cs
+++ TouchpointApp/TouchpointApp/ViewModel/Underviser/UnderviserViewmodelCollectionRediger.cs
@@ -4,22 +4,78 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TouchpointApp.DataStorage;
 using TouchpointApp.Persistency;
+using TouchpointApp.Model;
 
 namespace TouchpointApp.ViewModel.Underviser
 {
-//    public class UnderviserViewmodelCollectionRediger
-//    {
-//        public UnderviserCatalog _underviserCatalog;
+    public class UnderviserViewmodelCollectionRediger : ViewModelBase
+    {
+        private UnderviserCatalog _underviserCatalog;
+        private Model.Underviser _ItemIsSeleceted;
+        private ObservableCollection<Model.Underviser> _observableCollection;
 
-//        public ObservableCollection<Model.Underviser> CreateObservableCollection()
-//        {
-//            var Collection = new ObservableCollection<Model.Underviser>();
-//            foreach (var item in _underviserCatalog)
-//            {
-//                Collection.Add(item);
-//            }
-//            return Collection;
-//        }
-//    }
+
+        public UnderviserViewmodelCollectionRediger()
+        {
+            CreateObservableCollection();
+        }
+
+
+        //Skal lave vores katalog om til en obserablecollection, som vi kan bind vores listviews ItemsSource til. 
+        public void CreateObservableCollection()
+        {
+            //Der oprettes et ObservableCollection 
+            var Collection = new ObservableCollection<Model.Underviser>();
+
+            foreach (var item in _underviserCatalog.Getlist)
+            {
+                Collection.Add(item);
+            }
+            _observableCollection = Collection;
+        }
+
+        //Propperti til at binde listviewets isSeleceted propperty til. 
+        public Model.Underviser SelectedItemListview
+        {
+            get { return _ItemIsSeleceted; }
+            set
+            {
+                _ItemIsSeleceted = value;
+                OnPropertyChanged("Seleceted item er opdateret");
+            }
+        }
+
+        //propperty der skal bindes til listviewets ItemsSource
+        public ObservableCollection<Model.Underviser> Collection
+        {
+            get { return _observableCollection; }
+        }
+
+
+        // skal bindes til details viewet på rediger siden
+        public string Name { get { return _ItemIsSeleceted.Navn; } set { _ItemIsSeleceted.Navn = value; } }
+        public string Email { get { return _ItemIsSeleceted.Email; } set { _ItemIsSeleceted.Email = value; } }
+        public string Adresse { get { return _ItemIsSeleceted.Addresse; } set { _ItemIsSeleceted.Addresse = value; } }
+        public string Tlf { get { return _ItemIsSeleceted.Tlf; } set { _ItemIsSeleceted.Tlf = value; } }
+
+
+    }
+
+    //    public class UnderviserViewmodelCollectionRediger
+    //    {
+    //        public UnderviserCatalog _underviserCatalog;
+
+    //        public ObservableCollection<Model.Underviser> CreateObservableCollection()
+    //        {
+    //            var Collection = new ObservableCollection<Model.Underviser>();
+    //            foreach (var item in _underviserCatalog)
+    //            {
+    //                Collection.Add(item);
+    //            }
+    //            return Collection;
+    //        }
+    //    }
+
 }
