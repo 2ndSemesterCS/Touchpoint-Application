01eac4d08e728e841ffa2b628b679a3d80812f58
Nikolaj Dyring Jensen <nikodyring@gmail.com>
2017-12-12 11:39:19 +0100

    WebAPI Implementation

---
+++ TouchpointApp/TouchpointApp/Persistency/Catalog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TouchpointApp.PersistencyInterfaces;
+using TouchpointApp.Web;
+
+namespace TouchpointApp.Persistency
+{
+    public class Catalog
+    {
+        private List<int> _coll;
+        //private IDataSource _source;
+
+        public Catalog(List<int> coll /*,IDataSource source*/)
+        {
+            _coll = coll;
+            //_source = source;
+        }
+        
+    }
+}
