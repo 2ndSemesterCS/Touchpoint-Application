9d3119e27ae7e3b04e355a19cf93de6098c6589b
DunkRing <Chris31@live.dk>
2017-12-13 18:31:55 +0100

    OpretSerieView næsten back in the game
    asdasd

--- TouchpointApp/TouchpointApp/ViewModel/Serie/SerieViewModel.cs
+++ TouchpointApp/TouchpointApp/ViewModel/Serie/SerieViewModel.cs
@@ -4,6 +4,10 @@ using TouchpointApp.Command;
 using TouchpointApp.DataStorage;
 using TouchpointApp.Persistency;
 using Windows.UI.Xaml.Controls;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 
 namespace TouchpointApp.ViewModel.Serie
@@ -13,7 +17,7 @@ namespace TouchpointApp.ViewModel.Serie
         #region Instance Fields
         private SerieCatalog _serieCatalog;
         private SerieData _serieData;
-        private Model.Serie _selectedSerie;
+        private List<Model.Kursus> _kursusSerie;
 
         private KursusCatalog _kursusCatalog;
         private Model.Kursus _selectedKursus;
@@ -21,14 +25,15 @@ namespace TouchpointApp.ViewModel.Serie
         #endregion
 
         #region Constructor
-        
+
         public SerieViewModel()
         {
             _serieCatalog = SerieCatalog.Instance();
             _kursusCatalog = KursusCatalog.Instance();
 
             _serieData = new SerieData();
-            OpretNytSerieCommand = new RelayCommand(OpretNytSerie);
+            OpretNySerieCommand = new RelayCommand(OpretNySerie);
+            addSerie = new RelayCommand(AddKursusTilSerie);
         }
 
         #endregion
@@ -36,7 +41,8 @@ namespace TouchpointApp.ViewModel.Serie
 
         #region Properties
         public SerieData serieData { get { return _serieData; } set { _serieData = value; } }
-        public RelayCommand OpretNytSerieCommand { get; set; }
+        public RelayCommand OpretNySerieCommand { get; set; }
+        public RelayCommand addSerie { get; set; }
         #endregion
 
         #region CollectionSerie
@@ -55,12 +61,25 @@ namespace TouchpointApp.ViewModel.Serie
         {
             get { return CreateObservableCollectionSerie(); }
         }
+            
+        
 
-        public Model.Serie SelectedItemListviewSerie
+        public ObservableCollection<Model.Kursus> CreateObservableCollectionKursusSerie()
         {
-            get { return _selectedSerie; }
-            set { _selectedSerie = value; }
+            var Collection = new ObservableCollection<Model.Kursus>();
+            foreach (var item in _kursusSerie)
+            {
+                Collection.Add(item);
+            }
+            return Collection;
         }
+
+        public ObservableCollection<Model.Kursus> CollectionKursusTilSerie
+        {
+            get { return CreateObservableCollectionKursusSerie(); }
+        }
+
+
 
         #endregion
 
@@ -84,14 +103,30 @@ namespace TouchpointApp.ViewModel.Serie
         public Model.Kursus SelectedItemListviewKursus
         {
             get { return _selectedKursus; }
-            set { _selectedKursus = value; }
+            set
+            {
+                _selectedKursus = value;
+                OnPropertyChanged(nameof(UnderviserData));
+                addSerie.RaiseCanExecuteChanged();
+            }
         }
 
         #endregion
+        public void AddKursusTilSerie()
+        {
+            _serieData.KursusSerie = _kursusSerie;
 
-        public void OpretNytSerie()
+            _kursusSerie.Add(_selectedKursus);
+            CreateObservableCollectionKursusSerie();
+            OnPropertyChanged(nameof(CollectionKursusTilSerie));
+        }
+
+        public void OpretNySerie()
         {
             _serieCatalog.OpretSerie(_serieData.KursusSerie, _serieData.Beskrivelse);
+            CreateObservableCollectionSerie();
+            OnPropertyChanged(nameof(CollectionSerie));
+
         }
 
     }
